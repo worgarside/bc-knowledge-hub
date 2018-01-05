e39d4787793ec58adf09fb7c77f0c10a64d53e3b
Will Garside <worgarside@gmail.com>
2018-01-05 14:41:04 +0000

    Search results page styled properly

--- CustomBlogSearchProvider.php
+++ CustomBlogSearchProvider.php
@@ -6,11 +6,12 @@ class CustomBlogSearchProvider extends ResultsProvider
 {
     public function search()
     {
-
         $matching = DB::table('rainlab_blog_posts')
             ->join('rainlab_blog_posts_categories', 'rainlab_blog_posts.id', '=', 'rainlab_blog_posts_categories.post_id')
             ->join('rainlab_blog_categories', 'rainlab_blog_posts_categories.category_id', '=', 'rainlab_blog_categories.id')
             ->select(DB::raw('rainlab_blog_posts.*, rainlab_blog_categories.name as cat_name,  rainlab_blog_categories.slug as cat_slug'))
+            -> where('title', 'like', "%{$this->query}%")
+            -> orWhere('content', 'like', "%{$this->query}%")
             ->get();
 
         foreach ($matching as $match) {
@@ -20,6 +21,7 @@ class CustomBlogSearchProvider extends ResultsProvider
             $result->title     = $match->title;
             $result->excerpt   = $match->excerpt;
             $result->model     = $match;
+            $result->url       = "/knowledge-hub/$match->cat_slug/$match->slug";
             $result->meta      = [
                 'slug' => $match->slug,
                 'cat_name' => $match->cat_name,
